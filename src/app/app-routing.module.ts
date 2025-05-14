f6b4433fc836c41416fced308aa861c92e31c39c
RukuS3 <lucascea.leyton@icloud.com>
2025-05-13 20:19:51 -0400

    Acá ya hace el codigo en Git Hub

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',  
+    pathMatch: 'full',
+  },
+  
+  {
+    path: 'auth',
+    loadChildren: () => import('./seguridad/auth/auth.module').then(m => m.AuthPageModule),
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./seguridad/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./seguridad/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'detalle-publicacion',
+    loadChildren: () => import('./trabajos/detalle-publicacion/detalle-publicacion.module').then( m => m.DetallePublicacionPageModule)
+  },
+  {
+    path: 'chat-preview',
+    loadChildren: () => import('./contacto/chat-preview/chat-preview.module').then( m => m.ChatPreviewPageModule)
+  },
+  {
+    path: 'chat-open',
+    loadChildren: () => import('./contacto/chat-open/chat-open.module').then( m => m.ChatOpenPageModule)
+  },
+  {
+    path: 'miperfil',
+    loadChildren: () => import('./seguridad/miperfil/miperfil.module').then( m => m.MiperfilPageModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
