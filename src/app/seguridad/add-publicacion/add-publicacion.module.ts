82abd0af80e238f7c554c6f82ccabf0a62447781
V1cioxD <matias.ortega2004b@gmail.com>
2025-05-23 17:51:19 -0400

    publicaciones(backend)+ TablaFIREBASE
    ya funciona historial y agregar publicaciones, (falta eliminar y editar)

--- src/app/seguridad/add-publicacion/add-publicacion.module.ts
+++ src/app/seguridad/add-publicacion/add-publicacion.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
 import { IonicModule } from '@ionic/angular';
 
 import { AddPublicacionPageRoutingModule } from './add-publicacion-routing.module';
-
 import { AddPublicacionPage } from './add-publicacion.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule, 
     IonicModule,
     AddPublicacionPageRoutingModule
   ],
--- src/app/seguridad/crear-publicacion/crear-publicacion.page.ts
+++ src/app/seguridad/crear-publicacion/crear-publicacion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-crear-publicacion',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-publicacion.page.scss'],
 })
 export class CrearPublicacionPage implements OnInit {
+  publicaciones: any[] = [];
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {}
 
   ngOnInit() {
+    // Asegúrate de que el nombre coincide exactamente con la colección en Firestore: "Publicacion"
+    this.afs.collection('Publicacion').valueChanges().subscribe(data => {
+      this.publicaciones = data.map((pub: any) => ({
+        ...pub,
+        fecha: pub.fecha?.toDate ? pub.fecha.toDate() : pub.fecha // convierte timestamp a Date
+      }));
+    });
   }
-
 }
