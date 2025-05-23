0f33b7d9e1172dbc89782e04c2eeec21f6fa443e
V1cioxD <matias.ortega2004b@gmail.com>
2025-05-22 21:32:33 -0400

    mi perfil
    actualizacion y perfil casi terminado

--- src/app/seguridad/miperfil/miperfil.page.ts
+++ src/app/seguridad/miperfil/miperfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-miperfil',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./miperfil.page.scss'],
 })
 export class MiperfilPage implements OnInit {
+  telefono: string = '+56 987654321';
+  correo: string = 'correo@ejemplo.com';
+  direccion: string = 'Av. Siempre Viva 742';
 
-  constructor() { }
+  modoEdicion: boolean = false;
 
-  ngOnInit() {
+  editandoTelefono: boolean = false;
+  editandoCorreo: boolean = false;
+  editandoDireccion: boolean = false;
+
+  valoracion: number = 4.7; 
+
+
+
+  // Imagen de perfil por defecto
+  fotoPerfil: string = 'https://placehold.co/150x150';
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  async cambiarFotoPerfil() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Photos // Abre la galería
+      });
+
+      this.fotoPerfil = image.dataUrl;
+    } catch (error) {
+      console.error('Error al seleccionar imagen:', error);
+    }
   }
 
+guardarCambios() {
+  console.log('Teléfono:', this.telefono);
+  console.log('Correo:', this.correo);
+  console.log('Dirección:', this.direccion);
+
+
+  this.editandoTelefono = false;
+  this.editandoCorreo = false;
+  this.editandoDireccion = false;
+}
 }
